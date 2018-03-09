6583bfd554e40f830e12cee34ec4d0e913321e1a
maybecharisma <36387595+maybecharisma@users.noreply.github.com>
2018-03-09 22:15:27 +0200

    Update asuna.js

--- asuna.js
+++ asuna.js
@@ -748,15 +748,6 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-    var prefix = "!!"
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
 
 client.on("message", (message) => {
     if (message.content.startsWith('!!delete')) {
